170de15d063688cb9e24d01d3766695316fa568e
lucas-andrad <lafdesouza2002@outlook.com>
2022-06-05 04:31:48 -0300

    test: increment convert function tests

--- src/tests/convert.test.js
+++ src/tests/convert.test.js
@@ -7,6 +7,12 @@ describe('convert', () => {
         expect(object).toEqual({ a: 1, b: 2 });
     });
 
+    it('Should do anything if another type be passed', () => {
+        const object = { a: 1, b: 2 };
+        convert(object);
+        expect(object).toEqual({ a: 1, b: 2 }, 'foo');
+    });
+
     it('Should convert all values if value wont be passed', () => {
         const object = { a: 1, b: 2 };
         const expectedResult = { a: '1', b: '2' };
@@ -14,10 +20,31 @@ describe('convert', () => {
         expect(object).toEqual(expectedResult);
     });
 
-    it('Should convert only specified value', () => {
+    it('Should convert to string successfully', () => {
         const object = { a: 1, b: 2 };
         const expectedResult = { a: 1, b: '2' };
         convert(object, 'string', 'b');
+        expect(object).toEqual(expectedResult);
+    });
+    
+    it('Should convert to number successfully', () => {
+        const object = { a: 1, b: '2' };
+        const expectedResult = { a: 1, b: 2 };
+        convert(object, 'number', 'b');
+        expect(object).toEqual(expectedResult);
+    });
+
+    it('Should convert to date successfully', () => {
+        const object = { a: 1, b: '2' };
+        const expectedResult = { a: 1, b: 980992800000 };
+        convert(object, 'date', 'b');
+        expect(object).toEqual(expectedResult);
+    });
+    
+    it('Should convert to boolean successfully', () => {
+        const object = { a: 1, b: null };
+        const expectedResult = { a: 1, b: false };
+        convert(object, 'boolean', 'b');
         expect(object).toEqual(expectedResult);
     });
 });
