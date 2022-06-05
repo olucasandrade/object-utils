39e0424e7cf4e5126fb2c7114dfb4133c6478772
lucas-andrad <lafdesouza2002@outlook.com>
2022-06-05 04:32:54 -0300

    fix: adjust removeValue function tests

--- src/tests/removeValue.test.js
+++ src/tests/removeValue.test.js
@@ -3,7 +3,7 @@ const removeValue = require('../utils/removeValue.js');
 describe('removeValue', () => {
     it('Should not do anything if value not exists', () => {
         const object = { a: 1, b: 2, c: 3 };
-        removeValue(object, 4);
+        removeValue(object);
         expect(object).toEqual({ a: 1, b: 2, c: 3 });
     });
 
