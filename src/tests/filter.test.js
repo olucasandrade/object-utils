18662e4129083f4b651ea35ecdbee37f2b98a05a
lucas-andrad <lafdesouza2002@outlook.com>
2022-06-04 23:22:41 -0300

    test: create filter tests

---
+++ src/tests/filter.test.js
@@ -0,0 +1,10 @@
+const filter = require('../utils/filter.js');
+
+describe('filter', () => {
+    it('Should filter values by callback function', () => {
+        const object = { a: 1, b: 2, c: 3, d: 4 };
+        const expectedResult = { a: 1, c: 3 };
+        const result = filter(object, (e) => e % 2 === 0);
+        expect(result).toEqual(expectedResult);
+    });
+});
