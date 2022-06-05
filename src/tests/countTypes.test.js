a586c9eb10c0eed726244ed564fd763077b29fd5
lucas-andrad <lafdesouza2002@outlook.com>
2022-06-05 04:32:20 -0300

    fix: adjust countTypes function tests

--- src/tests/countTypes.test.js
+++ src/tests/countTypes.test.js
@@ -2,8 +2,8 @@ const countTypes = require('../utils/countTypes.js');
 
 describe('countTypes', () => {
     it('Should count types successfully', () => {
-        const object = { a: '1', b: 2, c: 3, d: [] };
-        const expectedResult = { 'string': 1, 'number': 2, 'array': 1 };
+        const object = { a: '1', b: 2, c: 3, d: [], e: undefined, f: null, g: null, h: true, i: {} };
+        const expectedResult = { 'string': 1, 'number': 2, 'array': 1, object: 1, null: 2, boolean: 1 };
         const result = countTypes(object);
         expect(result).toEqual(expectedResult);
     });
