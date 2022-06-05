a4a45a1154660870f5405a72646b24fe4e0a9a32
lucas-andrad <lafdesouza2002@outlook.com>
2022-06-04 23:55:51 -0300

    feat: create isEqual function

---
+++ src/utils/isEqual.js
@@ -0,0 +1,17 @@
+/**
+ * Verify if two objects is equal
+ * @param  {Object} object1 Object to be compared
+ * @param  {Object} object2 Object to be compared
+ * @return {Boolean}
+ */
+const isEqual = (object1, object2) => {
+    const keys1 = Object.keys(object1);
+    const keys2 = Object.keys(object2);
+    if (keys1.length !== keys2.length) return false;
+    for (let key of keys1) {
+      if (object1[key] !== object2[key]) return false;
+    }
+    return true;
+};
+
+module.exports = isEqual;
