72fe8555c271a75fc425ff0a268ac07b7e6c4bd3
lucas-andrad <lafdesouza2002@outlook.com>
2022-06-05 04:45:26 -0300

    feat: create index.js

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const addValue = require('./utils/addValue.js');
+const convert = require('./utils/convert.js');
+const copy = require('./utils/copy.js');
+const countTypes = require('./utils/countTypes.js');
+const filter = require('./utils/filter.js');
+const hasKey = require('./utils/hasKey.js');
+const hasValue = require('./utils/hasValue.js');
+const isDeepEqual = require('./utils/isDeepEqual.js');
+const isEmpty = require('./utils/isEmpty.js');
+const isEqual = require('./utils/isEqual.js');
+const isObject = require('./utils/isObject.js');
+const map = require('./utils/map.js');
+const merge = require('./utils/merge.js');
+const removeValue = require('./utils/map.js');
+const swapPairs = require('./utils/swapPairs.js');
+
+module.exports - {
+    addValue,
+    convert,
+    copy,
+    countTypes,
+    filter,
+    hasKey,
+    hasValue,
+    isDeepEqual,
+    isEmpty,
+    isEqual,
+    isObject,
+    map,
+    merge,
+    removeValue,
+    swapPairs,
+};
