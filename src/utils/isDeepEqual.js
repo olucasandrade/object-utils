93c5ab511bfa0a95f556137868886ca8dfd59689
lucas-andrad <lafdesouza2002@outlook.com>
2022-06-05 04:28:04 -0300

    docs: add jsdoc to isDeepEqual function

--- src/utils/isDeepEqual.js
+++ src/utils/isDeepEqual.js
@@ -1,5 +1,11 @@
 const isObject = require('./isObject');
 
+/**
+ * Verify if two objects are deeply equal
+ * @param  {Object} object1 Object to be compared
+ * @param  {Object} object2 Object to be compared
+ * @return {Boolean}
+ */
 const isDeepEqual = (object1, object2) => {
     const keys1 = Object.keys(object1);
     const keys2 = Object.keys(object2);
