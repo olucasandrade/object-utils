d94ac2ec624a79253457d4c921002c7e43076734
lucas-andrad <lafdesouza2002@outlook.com>
2022-06-05 04:32:37 -0300

    fix: adjust filter function tests

--- src/tests/filter.test.js
+++ src/tests/filter.test.js
@@ -4,7 +4,14 @@ describe('filter', () => {
     it('Should filter values by callback function', () => {
         const object = { a: 1, b: 2, c: 3, d: 4 };
         const expectedResult = { a: 1, c: 3 };
-        const result = filter(object, (e) => e % 2 === 0);
+        const result = filter(object, (e) => e % 2 !== 0);
         expect(result).toEqual(expectedResult);
+    });
+
+    it('Should filter original object if removeOriginal is true', () => {
+        const object = { a: 1, b: 2, c: 3, d: 4 };
+        const expectedResult = { a: 1, c: 3 };
+        filter(object, (e) => e % 2 !== 0, true);
+        expect(object).toEqual(expectedResult);
     });
 });
