443bcb76dcefce8a344f8a074c7c3bb6372d991c
lucas-andrad <lafdesouza2002@outlook.com>
2022-06-04 23:24:29 -0300

    test: create removeValue tests

---
+++ src/tests/removeValue.test.js
@@ -0,0 +1,16 @@
+const removeValue = require('../utils/removeValue.js');
+
+describe('removeValue', () => {
+    it('Should not do anything if value not exists', () => {
+        const object = { a: 1, b: 2, c: 3 };
+        removeValue(object, 4);
+        expect(object).toEqual({ a: 1, b: 2, c: 3 });
+    });
+
+    it('Should remove a value successfully', () => {
+        const object = { a: 1, b: 2, c: 3 };
+        const expectedResult = { a: 1, b: 2 };
+        removeValue(object, 3);
+        expect(object).toEqual(expectedResult);
+    });
+});
